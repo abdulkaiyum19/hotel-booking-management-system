c9a8ca3a33d80b32688b2cdff6f248ab333123e4
Abdul Kaiyum <140734510+abdulkaiyum19@users.noreply.github.com>
2026-05-10 23:50:43 +0600

    Update footer year and developer name

--- frontend/components/layout/footers.jsx
+++ frontend/components/layout/footers.jsx
@@ -14,14 +14,14 @@ function Footers() {
     <footer className='footer'>
       <h2>Beach Resort | Hotel Room Booking System</h2>
       <p>
-        ©2026 Beach Resort — Developed By
+        ©2023 Beach Resort — Developed By
         {' '}
         <a
           href='https://srmukul.com'
           rel='noreferrer'
           target='_blank'
         >
-          Abdul Kaiyum
+          Md. Samiur Rahman (Mukul)
         </a>
       </p>
     </footer>
